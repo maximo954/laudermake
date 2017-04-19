30945761ff85f940802ff78c75106f68121c21df
maximo954 <agphotography954@gmail.com>
2017-04-18 21:35:37 -0400

    Cleaned up code.

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -1,5 +1,5 @@
 class ToolsController < ApplicationController
-
+  before_action :set_tool_item, only: [:show, :edit, :update, :destroy, :checked_out, :return]
   before_action :authenticate_user!
   before_action :authorize_user!, only: [:new, :edit, :create]
 
@@ -8,7 +8,7 @@ class ToolsController < ApplicationController
   end
 
   def show
-    @tool = Tool.find(params[:id])
+
   end
 
   def new
@@ -17,7 +17,6 @@ class ToolsController < ApplicationController
 
   def create
     colors = [ 'black', 'blue', 'green', 'orange', 'red' ]
-
     @tool = Tool.new(tool_params)
     @tool.user_id = current_user.id
     @tool.label_color = colors.sample
@@ -32,11 +31,10 @@ class ToolsController < ApplicationController
   end
 
   def edit
-  @tool = Tool.find(params[:id])
+
   end
 
   def update
-    @tool = Tool.find(params[:id])
     @tool.user_id = current_user.id
     if @tool.update(tool_params)
       redirect_to root_path,
@@ -47,26 +45,27 @@ class ToolsController < ApplicationController
   end
 
   def destroy
-    @tool = Tool.find(params[:id])
     @tool.destroy
     redirect_to root_path
   end
 
   def checked_out
-    @tool = Tool.find(params[:id])
     @tool.toogle_check
     @tool.create_rental(tool_id: @tool.id, user_id: current_user.id)
     redirect_to root_path, notice: 'You have successfully rent a tool.'
   end
 
   def return
-    @tool = Tool.find(params[:id])
     @tool.toogle_check
     @tool.create_rental(tool_id: nil, user_id: nil)
     redirect_to root_path
   end
 
   private
+
+  def set_tool_item
+    @tool = Tool.find(params[:id])
+  end
 
   def tool_params
     params.require(:tool).permit(:name, :serial_number, :replacement_cost, :label_color, :user_id, :image )
